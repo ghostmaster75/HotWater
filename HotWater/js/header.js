63a41c8509b28e22418def04f97aeab9ad3a9592
Ghost <ghostmaster75@gmail.com>
2018-01-26 11:12:41 +0100

    menu

--- HotWater/js/header.js
+++ HotWater/js/header.js
@@ -1,13 +1,16 @@
 $('#menubox').hide();
 $('.headlogo').click(function() {
 	$('#menubox').slideToggle("fast");
+	$('#menuarrow').toggleClass("fa-angle-up", "fa-angle-down");
 });
 
 $(document).mouseup(function(e) {
 	var container = $('#menubox');
 	var menubtn = $('.headlogo');
+	var menuarrow = $('#menuarrow');
 
 	if (!container.is(e.target) && container.has(e.target).length === 0 && !menubtn.is(e.target) && menubtn.has(e.target).length === 0) {
 		container.slideUp("fast");
+		menuarrow.toggleClass("fa-angle-up", "fa-angle-down");
 	}
 });
--- HotWater/templates/header.ctp
+++ HotWater/templates/header.ctp
@@ -2,7 +2,7 @@
 <!-- script: js/header.js -->
 <div id="header">
 	<ul>
-		<li class='headlogo'><i class="fas fa-server"></i><h3>{title}</h3></li>
+		<li class='headlogo'><i id="menuarrow" class="fas fa-angle-down"></i><i class="fas fa-server"></i><h3>{title}</h3></li>
 		<li class='userbox'><i class="far fa-user"></i><span>{username}</span></li>		
 	</ul>
-</div>
+</div>	
